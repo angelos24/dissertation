9be107c4c5a2186c90d3fd06544be0492aef626f
angelos24 <aathan93@hotmail.com>
2018-09-24 15:06:06 +0100

    fixed map and changed dropdown layout

--- Sourcecode/app.py
+++ Sourcecode/app.py
@@ -1,46 +1,66 @@
 import dash, plotly
 import dash_html_components as html
 import dash_core_components as dcc
-from flask  import Flask, url_for, abort, request, render_template, json, flash, redirect, session, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+
 
 app = dash.Dash()
 
 external_css = ["https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
                 "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", ]
 
+mapbox_access_token = "pk.eyJ1IjoiYWF0aGFuOTMiLCJhIjoiY2psdGVqdGhrMDVnaTNrbDgyZW9mZjBncSJ9.xQBdSA7YMBS_1Lnkj2nZ5Q"
+
 for css in external_css:
     app.css.append_css({"external_url": css})
 
-
+df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\leuchar.csv')
+print(df)
 
 app.layout = html.Div([
-	html.H2("Scotland Weather Data ", style={'font-family': 'Dosis'}),
+	html.H2("Scotland Weather Data"),
 
 
-	 dcc.Dropdown(
-                id='data_drop',
-                options=[
-                    {'label': 'Max Temperature', 'value': 'tmax'},
-                    {'label': 'Min Temperature', 'value': 'tmin'},
-                    {'label': 'Rain (mm)', 'value': 'rain'},
-                    {'label': 'Sun (hours)', 'value': 'sun'},
+	    dcc.Graph(
+            id = "mapbox",
+            figure={
+                "data": [
+                    dict(
+                        type = "scattermapbox",
+                        mode = "markers",
+                        marker = {'size': '14'},
+                    )
                 ],
-                value="rain",
-                placeholder="Please choose a weather phenomeno",
-                className="data_option"
-            ),
-	  dcc.Graph(id='map', figure={
-        'layout': {
-            'mapbox': {
-                'accesstoken': 'pk.eyJ1IjoiYWF0aGFuOTMiLCJhIjoiY2psdGVqdGhrMDVnaTNrbDgyZW9mZjBncSJ9.xQBdSA7YMBS_1Lnkj2nZ5Q'
+                "layout": dict(
+                    hovermode = "closest",
+                    margin = dict(l = 0, r = 0, t = 0, b = 0),
+                    mapbox = dict(
+                        accesstoken = mapbox_access_token,
+                        center = dict(lat = 38.30, lon = -90.68),
+                        style = "dark",
+                        zoom = 3.5,
+                        layers = []
+                    )   
+                )
             },
-            'hovermode': 'closest',
-            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
-        }
-    }),
+        ),
+        dcc.RangeSlider(
+        className='my-range-slider',
+        min=0,
+        max=20,
+        step=0.5,
+        value=[5, 15]
+    ),
 
-	], className='container')
+        html.Div([
+        	html.Div([
+        	dcc.Graph(
+        		 id='temp_graph',
+        		 config={'displayModeBar': False})], className='col-md-4'),
+			html.Div([dcc.Graph(id='sun_graph')], className='col-md-4'),
+			html.Div([dcc.Graph(id='rain_graph')], className='col-md-4'),
+		], className='row')
+	], className='container-fluid')
 
 
 
