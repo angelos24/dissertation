1657b4660d61498d6b702bace9b3bca5ad70f508
angelos24 <aathan93@hotmail.com>
2018-10-23 15:41:42 +0100

    range slider outputs each year value

--- Sourcecode/app.py
+++ Sourcecode/app.py
@@ -3,6 +3,8 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
 import json
 
 
@@ -18,7 +20,18 @@ for css in external_css:
     app.css.append_css({"external_url": css})
 
 df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\leuchar.csv')
+# df.sort_values(by=['yyyy'])
+
 print(df)
+
+
+# dfmax = df['tmax'].groupby(df['yyyy']).describe()
+
+
+
+
+
+
 
 app.layout = html.Div([
 	html.H2("Scotland Weather Data"),
@@ -32,6 +45,7 @@ app.layout = html.Div([
                     	lat = ['56.38'],
                     	lon =  ['-2.88'],
                         type = "scattermapbox",
+                        customdata = df['tmax'],
                         mode = "markers",
                         marker = {'size': '14',},
                         text = ['Leuchar']
@@ -61,22 +75,23 @@ app.layout = html.Div([
 
 		
     ),
+        html.H3(id='test', style={'color': 'white'}),
+
+
      html.Div([
      	html.H3(id='year-range', style={'color': 'white'}),
 		], className='row'),
-
-
-        html.Div(
-        html.Pre(id='test', style={'overflowY': 'scroll', 'height': '100vh', 'color': 'white'})
-    ),
 
         html.Div([
         	html.Div([
         	dcc.Graph(
         		 id='temp_graph',
-        		 config={'displayModeBar': False})], className='col-md-4'),
-			html.Div([dcc.Graph(id='sun_graph')], className='col-md-4'),
-			html.Div([dcc.Graph(id='rain_graph')], className='col-md-4'),
+        		 config={'displayModeBar': False})], className='col-md-12'),
+			# html.Div([
+			# 	dcc.Graph(
+			# 		id='sun_graph',
+			# 		config={'displayModeBar': False})], className='col-md-6'),
+			# html.Div([dcc.Graph(id='rain_graph')], className='col-md-4'),
 		], className='row')
 	],  className='container-fluid')
 
@@ -86,20 +101,148 @@ app.layout = html.Div([
     [Input('my-range-slider', 'value')]
 )
 
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
+def update_output(year_range):
+	date_start = '{}-01-01'.format(year_range[0])
+	date_end = '{}-12-31'.format(year_range[1])
+	yearvalues = pd.date_range(start=date_start ,end=date_end,freq='Y')
+	years = yearvalues.year
+	return str(years)
 
+
+
+
+
+# @app.callback(
+#     	Output('test', 'children'),
+#     	[Input('my-range-slider', 'value')]
+#     	)
+
+# def test(year_range):
+#     dff = df[df['year'] == year_range]
+#     return '{} joules'.format(str(dff['tmax'].max()))
 
 
 @app.callback(
-        Output('test', 'children'),
-        [Input('mapbox', 'clickData')])
+        Output('temp_graph', 'figure'),
+        [Input('mapbox', 'clickData'),
+         Input('my-range-slider', 'value')])
 
-def display_data(clickData):
-    return json.dumps(clickData, indent=2)
+def update_graph( clickData, year_range):
 
+    data = go.Data([
+        go.Scatter(
+            name='Max temperature',
+            # events qty
+            x=year_range,
+            # year
+            y=[df['tmax']],
+
+            mode='lines',
+            marker={
+                'symbol': 'circle',
+                'size': 5,
+                'color': '#eb1054'
+            },
+            hoverlabel={
+                'bgcolor': '#FFF',
+            },
+        ),
+        go.Scatter(
+            name='Min temperature',
+            # events qty
+            x=year_range,
+            # year
+            y=df['tmin'],
+            mode='lines',
+            marker={
+                'symbol': 'circle',
+                'size': 5,
+                'color': '#C2FF0A'
+            },
+            hoverlabel={
+                'bgcolor': '#FFF',
+            },
+        ),
+    ])
+    layout = go.Layout(
+        xaxis={
+            'color': '#FFF',
+            'title': 'year',
+        },
+        yaxis={
+            'color': '#FFF',
+            'title': 'temperature range',
+        },
+        margin={
+            'l': 40,
+            'b': 40,
+            't': 10,
+            'r': 0
+        },
+        hovermode='closest',
+        paper_bgcolor='#191a1a',
+        plot_bgcolor='#191a1a',
+    )
+
+    return go.Figure(
+        data=data,  
+        layout=layout
+    )
+
+
+
+    @app.callback(
+        Output('sun_graph', 'figure'),
+        [Input('mapbox', 'clickData'),
+         Input('my-range-slider', 'value')])
+
+    def sun_chart(clickData):
+
+    	data = go.Data([
+        go.Scatter(
+            name='Sun',
+            # events qty
+            x=year_range,
+            # year
+            y=df['sun'],
+
+            mode='bar',
+            marker={
+                'symbol': 'circle',
+                'size': 5,
+                'color': '#eb1054'
+            },
+            hoverlabel={
+                'bgcolor': '#FFF',
+            },
+        ),
+    ])
+    layout = go.Layout(
+        xaxis={
+            'color': '#FFF',
+            'title': 'year',
+        },
+        yaxis={
+            'color': '#FFF',
+            'title': 'Sunshine hours',
+        },
+        margin={
+            'l': 40,
+            'b': 40,
+            't': 10,
+            'r': 0
+        },
+        hovermode='closest',
+        paper_bgcolor='#191a1a',
+        plot_bgcolor='#191a1a',
+    )
+
+    return go.Figure(
+        data=data,  
+        layout=layout
+    )
 
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+	app.run_server(debug=True)
