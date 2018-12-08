08ffc0dc7905e49e602ff6432688aed800a56e2b
angelos24 <aathan93@hotmail.com>
2018-12-08 14:35:10 +0000

    final commit

--- Sourcecode/app.py
+++ Sourcecode/app.py
@@ -21,61 +21,72 @@ for css in external_css:
     app.css.append_css({"external_url": css})
 
 df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\leuchar.csv')
+test = df.loc[df['yyyy'].idxmin()]
+print(test)
 
 
 app.layout = html.Div([
-	html.H2("Scotland Weather Data"),
-
-
-	    dcc.Graph(
-            id = "mapbox",
-            figure={
-                "data": [
-                    dict(
-                    	lat = ['56.38','57.593'],
-                    	lon =  ['-2.88','-3.821'],
-                        type = "scattermapbox",
-                        customdata = df['tmax'],
-                        mode = "markers",
-                        marker = {'size': '14',},
-                        text = ['leuchar','nairn']
-                    )
-                ],
-                "layout": dict(
-                    hovermode = "closest",
-                    margin = dict(l = 0, r = 0, t = 0, b = 0),
-                    mapbox = dict(
-                        accesstoken = mapbox_access_token,
-                        center = dict(lat = 56.49, lon = -4.20),
-                        style = "dark",
-                        zoom = 6,
-                        layers = []
-                    )   
-                )
-            },
-        ),
+	html.H2("Scotland Weather Data"),    
+    html.Div([
         html.Div([
-            html.H4('Showing data for:'),
+            html.Div([
+                dcc.Graph(
+                    id = "mapbox",
+                    figure={
+                        "data": [
+                            dict(
+                                lat = ['56.38','57.593','57.0098'],
+                                lon =  ['-2.88','-3.821','-3.3979'],
+                                type = "scattermapbox",
+                                customdata = df['tmax'],
+                                mode = "markers",
+                                marker = {'size': '14',},
+                                text = ['leuchar','nairn','braemar']
+                                )
+                                ],
+                                "layout": dict(
+                                    hovermode = "closest",
+                                    margin = dict(l = 0, r = 0, t = 0, b = 0),
+                                    mapbox = dict(
+                                        accesstoken = mapbox_access_token,
+                                        center = dict(lat = 56.49, lon = -4.20),
+                                        style = "dark",
+                                        zoom = 6,
+                                        layers = []
+                                    )   
+                                )
+                    },
+                ),
+
+            ], className='col-md-6'),
+            html.Div([
+                html.H3('Click on one of the dots to visualise the desired weather station',  id = 'test'),
+                html.Hr(),
+                html.Div([
+                html.Div([html.H5(id = 'high_temp')], className='col-md-12'),
+                html.Div([html.H5(id = 'low_temp')], className='col-md-12'),
+                html.Div([html.H5(id = 'high_sunshine')], className='col-md-12'),
+                html.Div([html.H5(id = 'low_sunshine')], className='col-md-12'),
+                html.Div([html.H5(id = 'high_perc')], className='col-md-12'),
+                html.Div([html.H5(id = 'low_perc')], className='col-md-12'),
+                html.Div([html.H5(id = 'years')], className='col-md-12'),
+
+                ], className='row'),
+            ], className='col-md-6 stats stats2' ),
+        ], className='row'),
+    ], className='container-fluid'),
+
+        html.Div([
             dcc.RangeSlider(
                 className = 'my-range-slider',
                 id = 'my-range-slider',
                 min = df['yyyy'].min(),
                 max = df['yyyy'].max(),
-                marks = {str(date): str(date)
-                for date in df['yyyy'].unique()},
-                value=[1970, 1993],
+                # marks = {str(date): str(date)
+                # for date in df['yyyy'].unique()},
+                value=[1980, 1993],
                 ),
-            html.Div([
-                html.Div([html.H5('Max Temperature')], className='col-md-4'),
-                html.Div([html.H5('Low Temperature')], className='col-md-4'),
-                html.Div([html.H5('High Percipitation')], className='col-md-4'),
-                ], className='row'),
-            ], className='container-fluid'),
-
-
-     html.Div([
-     	html.H3('test', id='year-range'),
-		], className='row'),
+            ], className='container-fluid stats2'),
 
         html.Div([
         	html.Div([
@@ -95,19 +106,116 @@ app.layout = html.Div([
 
 
 @app.callback(
-    Output('year-range', 'children'),
+    Output('low_perc', 'children'),
     [Input('mapbox', 'clickData')]
 )
 
-def showstation(clickData):
+def getlowperc(clickData):
     point = clickData['points'][0]
-    return pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['rain'].idxmin()]
+    rain = int(filter['rain'])
+    month = int(filter['mm'])
+    year = int(filter['yyyy'])
+    return 'Lowest percipitation (in MM) '+str(rain)+ ' in '+str(month)+"/"+str(year)
 
-# test = 'leuchar'
 
-# df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ test +'.csv')
+@app.callback(
+    Output('high_perc', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def gethighperc(clickData):
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['rain'].idxmax()]
+    rain = int(filter['rain'])
+    month = int(filter['mm'])
+    year = int(filter['yyyy'])
+    return 'Highest percipitation (in MM)  were '+str(rain)+ ' in '+str(month)+"/"+str(year)
 
 
+@app.callback(
+    Output('low_sunshine', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def getlowsun(clickData):
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['sun'].idxmin()]
+    sun = int(filter['sun'])
+    month = int(filter['mm'])
+    year = int(filter['yyyy'])
+    return 'Lowest sunshine hours were '+str(sun)+ ' in '+str(month)+"/"+str(year)
+
+
+
+@app.callback(
+    Output('high_sunshine', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def gethighsun(clickData):
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['sun'].idxmax()]
+    sun = int(filter['sun'])
+    month = int(filter['mm'])
+    year = int(filter['yyyy'])
+    return 'Highest sunshine hours were '+str(sun)+ ' in '+str(month)+"/"+str(year)
+
+
+@app.callback(
+    Output('low_temp', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def getlowtemp(clickData):
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['tmin'].idxmin()]
+    temp = filter['tmin']
+    month = filter['mm']
+    year = filter['yyyy']
+    return 'Lowest temperature was '+str(int(temp))+ ' in '+str(int(month))+"/"+str(int(year))
+
+
+@app.callback(
+    Output('years', 'children'),
+    [ Input('my-range-slider', 'value')]
+)
+
+def get_yearange(year_range):
+    date_start = '{}'.format(year_range[0])
+    date_end = '{}'.format(year_range[1])
+    return 'The default dates shown are from '+date_start+ ' until ' +date_end
+
+
+@app.callback(
+    Output('high_temp', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def gethightemp(clickData):
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
+    filter = df.loc[df['tmax'].idxmax()]
+    temp = int(filter['tmax'])
+    month = int(filter['mm'])
+    year = int(filter['yyyy'])
+    return 'Highest temperature was '+str(temp)+ ' in '+str(month)+"/"+str(year)
+
+
+@app.callback(
+    Output('test', 'children'),
+    [Input('mapbox', 'clickData')]
+)
+
+def getstationname(clickData):
+    point = clickData['points'][0]
+    station = point['text']
+    return 'Showing data for '+station
 
 
 @app.callback(
@@ -115,7 +223,7 @@ def showstation(clickData):
         [Input('mapbox', 'clickData'),
          Input('my-range-slider', 'value')])
 
-def update_graph(clickData, year_range):
+def update_graph(clickData, year_range,):
     point = clickData['points'][0]
     df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
     date_start = '{}'.format(year_range[0])
@@ -157,12 +265,14 @@ def update_graph(clickData, year_range):
 
     return {
     'data': [lower_bound, trace, upper_bound],
-    'layout': dict(plot_bgcolor='black', paper_bgcolor='black', title = 'Max and Low Temperatures in Leuchar',
+    'layout': dict(plot_bgcolor='black', paper_bgcolor='black', title = 'Max and Low Temperatures in '+ point['text'] +'',
               xaxis = dict(title = 'Year Range', gridcolor='rgba(0, 0, 0, 0.9)',),
               yaxis = dict(title = 'Temperature (degrees Celsius)',gridcolor='rgba(0, 0, 0, 0.9)',),
               font=dict(size=14, color='#7f7f7f'),
               )
 	}
+
+    
 
 
 @app.callback(
@@ -170,8 +280,10 @@ def update_graph(clickData, year_range):
         [Input('mapbox', 'clickData'),
          Input('my-range-slider', 'value')])
 
-def update_graph(clickData, year_range):
+def update_graph(clickData, year_range,):
 
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
     date_start = '{}'.format(year_range[0])
     date_end = '{}'.format(year_range[1])
     yearvalues = pd.date_range(start=date_start ,end=date_end,freq='M')
@@ -189,9 +301,9 @@ def update_graph(clickData, year_range):
 
     return {
     'data': [trace1],
-    'layout': dict(plot_bgcolor='black', paper_bgcolor='black', title = 'Percipitation in Leuchar',
-              xaxis = dict(title = 'Year Range', gridcolor='black',),
-              yaxis = dict(title = 'Rain (MM)',gridcolor='black',),
+    'layout': dict(plot_bgcolor='rgba(0, 0, 0, 0.9', paper_bgcolor='rgba(0, 0, 0, 0.9', title = 'Percipitation '+ point['text'] +'',
+              xaxis = dict(title = 'Year Range', gridcolor='rgba(0, 0, 0, 0.9',),
+              yaxis = dict(title = 'Rain (MM)',gridcolor='rgba(0, 0, 0, 0.9',),
               font=dict(size=14, color='#7f7f7f'),
 
               )
@@ -204,6 +316,8 @@ def update_graph(clickData, year_range):
 
 def update_graph(clickData, year_range):
 
+    point = clickData['points'][0]
+    df = pd.read_csv('C:\\Users\\aathan\\Desktop\\dissertation\\data\\'+ point['text'] +'.csv')
     date_start = '{}'.format(year_range[0])
     date_end = '{}'.format(year_range[1])
     yearvalues = pd.date_range(start=date_start ,end=date_end,freq='M')
@@ -233,7 +347,7 @@ def update_graph(clickData, year_range):
 
     return {
     'data': [trace1,trace2],
-    'layout': dict(plot_bgcolor='black', paper_bgcolor='black', title = 'Total Monthly Sunshine & Air Frost in Leuchar',
+    'layout': dict(plot_bgcolor='black', paper_bgcolor='black', title = 'Total Monthly Sunshine & Air Frost in '+ point['text'] +'',
               xaxis = dict(title = 'Year Range', gridcolor='black',),
               yaxis = dict(title = 'Sunshine hours',gridcolor='black',),
               font=dict(size=14, color='#7f7f7f'),
